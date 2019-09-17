2653c9b3b9562fea5135d409e04e268d2fd6bf8d
João Medeiros <joaoserj@gmail.com>
2019-09-17 02:34:29 -0300

    update about

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
+    <q-card>
+      <q-card-section class="text-center">
+        <div class="text-h6">
+          This software was developed by João Medeiros, student of Systems Analysis and Development for study purposes. <br><br> 
+          Este software foi desenvolvido por João Medeiros, estudante de Análise e Desenvolvimento de Sistemas para fins de estudo.
+        </div>
+        <br>
 
+        <div class="text-h5">2019</div>
+        
+      </q-card-section>
+      <q-card-section class="text-center">
+        <q-btn label="Go Back to Home" color="green" to="stickynotes" />
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
